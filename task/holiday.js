b540b2c023ba91c9b6cee26d6b7284efabe492c8
htoooth <ht.anglenx@gmail.com>
2018-02-27 10:13:01 +0800

    fix

--- task/holiday.js
+++ task/holiday.js
@@ -4,7 +4,7 @@ const END_POINT = 'http://holiday-api.leanapp.cn/api/v1/work'
 
 async function getWorkday(date) {
   return new Promise(function(resolve, reject) {
-    request.get(END_POINT, {qs: {date}}, function(err, res, body) {
+    request.get(END_POINT, {qs: {date}, json:true}, function(err, res, body) {
       if (err) {
         reject(err)
         return
