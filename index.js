9a85a252da877e4d6dbf5072bef43c29a04fd9cc
htoooth <ht.anglenx@gmail.com>
2018-02-27 11:04:20 +0800

    fix

--- index.js
+++ index.js
@@ -2,19 +2,33 @@ const schedule = require('node-schedule')
 const checkin = require('./checkin')
 const checkout = require('./checkout')
 
+function sleep(ms) {
+  return new Promise(function(resolve) {
+    setTimeout(() => {
+      resolve()
+    }, ms)
+  })
+}
+
+const TWENTY_MINUTES = 10 * 60 * 1000
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function main() {
   const ruleIn = new schedule.RecurrenceRule()
   ruleIn.hour = 9
 
   schedule.scheduleJob(ruleIn, function(){
-    checkin()
+    sleep(getRandomInt(TWENTY_MINUTES)).then(checkin)
   });
 
   const ruleOut = new schedule.RecurrenceRule()
   ruleOut.hour = 18
 
   schedule.scheduleJob(ruleOut, function(){
-    checkout()
+    sleep(getRandomInt(TWENTY_MINUTES)).then(checkout)
   });
 
   console.log('checkin start')
