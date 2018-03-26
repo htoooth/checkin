02688a8ccf7045a5a7048403d4dfd4c56caa493e
htoooth <ht.anglenx@gmail.com>
2018-03-26 16:52:44 +0800

    fix

--- schedule.js
+++ schedule.js
@@ -3,17 +3,22 @@ const service = require('./server/service')
 
 function scheduleMain() {
   const ruleIn = new schedule.RecurrenceRule()
-  ruleIn.hour = 8
-  ruleIn.minute = 40
+  // ruleIn.hour = 8
+  // ruleIn.minute = 40
+
+  ruleIn.hour = 17
+  ruleIn.minute = 0
 
   schedule.scheduleJob(ruleIn, function(){
     service.checkinAll()
   });
 
   const ruleOut = new schedule.RecurrenceRule()
-  ruleOut.hour = 18
-  ruleOUt.minute = 0
+  // ruleOut.hour = 18
+  // ruleOUt.minute = 0
 
+  ruleOut.hour = 17
+  ruleOUt.minute = 0
   schedule.scheduleJob(ruleOut, function(){
     service.checkoutAll()
   });
